fdcb6abddfbd56ab58eba1b852c3247f7e515c23
pranita R <pranitabhatkar35@gmil.com>
2026-01-13 13:35:43 +0530

    if-else problem

---
+++ if-else problem.py
@@ -0,0 +1,51 @@
+#cheking positive or negative number
+
+# num=int(input("Enter a number:"))
+# if num>0:
+#     print("Positive number")
+# else:
+#    print("Negative number")
+
+#check correct OTP
+
+# corrct_OTP=2804
+# OTP=int(input("Enter a OTP:"))
+# if corrct_OTP == OTP:
+#    print("Successfully complited!")
+# else:  
+#   print("Failed! try again")
+
+#marks on grade based
+
+# marks=int(input("Enter your marks:"))
+# if marks>=90:
+#     print( "grade A")
+# elif marks>=75:
+#     print("grade B")
+# elif marks>=60:
+#     print("grade C")
+# else:
+#     print("Fail")  
+
+#Age catergory based on age
+
+# age=int(input("enter your age:"))
+# if age<13:
+#     print("Child")
+# elif age<20:
+#     print("Teenager")  
+
+# elif age<60:
+#     print("Adult")
+# else:
+#     print("Senior Citizen") 
+
+# Electricity bill category based on units
+
+# units=int(input("Enter the units:"))
+# if units<100:
+#     print("Low Usage")
+# elif units<300:
+#     print("Medium Usage") 
+# else:
+#     print("High Usage")      
